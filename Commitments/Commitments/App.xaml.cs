2408590b620a43a20c2d3500ef842d1915bb74cf
David Nyman <david.nyman@protonmail.com>
2023-02-02 13:17:07 +0100

    Add config

--- Commitments/Commitments/App.xaml.cs
+++ Commitments/Commitments/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,28 @@ namespace Commitments
     /// </summary>
     public partial class App : Application
     {
+        public IConfigurationRoot? Config = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Config = LoadConfig();
+            base.OnStartup(e);
+        }
+
+        private static IConfigurationRoot? LoadConfig()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                return config;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+        }
     }
 }
